069982b6adcc80ccc09ddc33902a234428a5b364
betogroo <luizhumberto@gmail.com>
2021-10-27 19:42:34 -0300

    feat: login with google

--- src/components/app/AppBtn.vue
+++ src/components/app/AppBtn.vue
@@ -5,8 +5,48 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+type BtnType = 'button' | 'submit'
 export default defineComponent({
-  name: 'AppBtn'
+  name: 'AppBtn',
+
+  props: {
+    type: {
+      type: String as PropType<BtnType>,
+      default: 'button'
+    }
+  }
 })
 </script>
+
+<style lang="scss" scoped>
+button {
+  padding: 8px;
+  box-sizing: border-box;
+  background-color: rgb(157, 157, 177);
+  margin-top: 4px;
+  margin-bottom: 4px;
+  border: 1px solid black;
+  border-radius: 4px;
+  color: white;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  height: 36px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+}
+button:not(:disabled):hover {
+  transform: scale(1.02);
+}
+
+button.block {
+  width: 100%;
+}
+button:disabled {
+  opacity: 0.3;
+  cursor: not-allowed;
+}
+</style>
--- src/modules/core/auth/views/Welcome.vue
+++ src/modules/core/auth/views/Welcome.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <h1>Forms</h1>
-    <LoginForm v-if="mode === 'login'" />
-    <hr />
-    <SignupForm v-if="mode === 'signup'" />
-    <hr />
-    <ResetPasswordForm v-if="mode === 'reset'" />
+  <div class="box">
+    <div class="form-box">
+      <h1>Login</h1>
+      <LoginForm v-if="mode === 'login'" />
+      <SignupForm v-if="mode === 'signup'" />
+      <ResetPasswordForm v-if="mode === 'reset'" />
+    </div>
   </div>
 </template>
 
@@ -37,4 +37,24 @@ export default defineComponent({
 })
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  height: 84vh;
+}
+.form-box {
+  border: 1px solid #000;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+  align-items: center;
+  padding: 4px;
+  width: 70vw;
+  max-width: 320px;
+  height: 200px;
+}
+</style>
