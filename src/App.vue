81e2a6758e0074dc597c0e1c032697520290084e
betogroo <luizhumberto@gmail.com>
2021-10-28 14:04:57 -0300

    feat: installl tailwind

--- src/App.vue
+++ src/App.vue
@@ -8,25 +8,13 @@
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+import './assets/tailwind.css'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default defineComponent({
+  setup() {
+    return {}
   }
-}
-</style>
+})
+</script>
